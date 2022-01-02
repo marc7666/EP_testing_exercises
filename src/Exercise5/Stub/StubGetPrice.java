a548e1c8fd2dad80b63d3372027d27b754363312
marc7666 <64266158+marc7666@users.noreply.github.com>
2022-01-02 20:02:58 +0100

    Stub implemented

--- src/Exercise5/Stub/StubGetPrice.java
+++ src/Exercise5/Stub/StubGetPrice.java
@@ -9,6 +9,14 @@ public class StubGetPrice implements ProductsDB {
 
     @Override
     public BigDecimal getPrice(String productID) throws DoesNotExistException {
-        return null;
+        if (productID.equals("Shotgun")) {
+            return new BigDecimal("1560.36");
+        } else if (productID.equals("Rifle")) {
+            return new BigDecimal("4897.12");
+        } else if (productID.equals("Revolver")) {
+            return new BigDecimal("2000.0");
+        } else {
+            throw new DoesNotExistException("This ID is not in the DB");
+        }
     }
 }
